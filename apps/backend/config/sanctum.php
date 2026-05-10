8bb784c500089809972d20a89d9a3eff2024780b
M-Abrar <m.abrar.hassan@gmail.com>
2026-05-10 21:33:21 +0500

    feat: initialize Sanctum API authentication configuration with custom token prefix and stateful domains

--- apps/backend/config/sanctum.php
+++ apps/backend/config/sanctum.php
@@ -53,7 +53,7 @@ return [
     |
     */
 
-    'expiration' => null,
+    'expiration' => env('SANCTUM_EXPIRATION', 525600), // Default 1 Year
 
     /*
     |--------------------------------------------------------------------------
@@ -68,7 +68,7 @@ return [
     |
     */
 
-    'token_prefix' => env('SANCTUM_TOKEN_PREFIX', ''),
+    'token_prefix' => env('SANCTUM_TOKEN_PREFIX', 'sellio_'),
 
     /*
     |--------------------------------------------------------------------------
