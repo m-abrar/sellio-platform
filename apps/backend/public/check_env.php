79a092c905d55acd5c8c7962b0635383cea6bf40
M-Abrar <m.abrar.hassan@gmail.com>
2026-05-14 23:52:44 +0500

    refactor: remove redundant file system utility module

--- apps/backend/public/check_env.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "PHP_SAPI: " . PHP_SAPI . "\n";
-echo "PHP_BINARY: " . PHP_BINARY . "\n";
-echo "register_argc_argv: " . ini_get('register_argc_argv') . "\n";
-echo "exec('php -v'): " . shell_exec('php -v') . "\n";
