faef21f21dd743612f71711dd1d2331cc820b42e
M-Abrar <m.abrar.hassan@gmail.com>
2026-05-26 18:08:34 +0500

    chore: update package-lock.json and server configuration
    - Added "peer": true to relevant dependencies in package-lock.json for better compatibility.
    - Updated several package versions in package-lock.json to their latest releases, ensuring improved stability and security.
    - Modified server.ts to dynamically set the PORT using process.env, enhancing deployment flexibility.

--- apps/buyer/server.ts
+++ apps/buyer/server.ts
@@ -197,7 +197,7 @@ seedDatabase();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT || 3000);
 
   app.use(express.json());
 
